742e7b70cfcede1406bf40033dd7b456eb58b8ed
Jan Eglinger <jan.eglinger@fmi.ch>
2025-02-17 15:09:28 +0100

    Fix image loading
    Do not overwrite _load_image at every iteration!

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -11,9 +11,10 @@ def cv_acquisition():
 
 def test_xarray_default(cv_acquisition):
     if HAS_XARRAY:
-        array = cv_acquisition.to_dataarray()
+        array = cv_acquisition.to_dataarray().compute()
         assert array.dims == ("row", "column", "field", "channel", "z", "y", "x")
         assert array.shape == (3, 2, 4, 2, 4, 2000, 2000)
+        assert cv_acquisition.to_dataarray().data.chunksize == (1, 1, 1, 1, 1, 2000, 2000)
     else:
         with pytest.raises(ValueError):
             cv_acquisition.to_dataarray()
@@ -27,15 +28,13 @@ def test_xarray_subset(cv_acquisition):
             fields=[1, 3, 4],
             channels=[1, 2],
             z_indices=[2, 3],
-        )
+        ).compute()
         assert array.dims == ("row", "column", "field", "channel", "z", "y", "x")
         assert array.shape == (1, 1, 3, 2, 2, 2000, 2000)
-        assert array.data.chunksize == (1, 1, 1, 1, 2, 2000, 2000)
         squeezed = array.squeeze()
         assert squeezed.dims == ("field", "channel", "z", "y", "x")
         assert squeezed.shape == (3, 2, 2, 2000, 2000)
         assert squeezed.dtype == "uint16"
-        assert squeezed.data.compute().dtype == "uint16"
     else:
         with pytest.raises(ValueError):
             cv_acquisition.to_dataarray()
